e7ca4807bf5bdc5019196213b53331b68a78a3a7
Boris Fox <boris@fox.khv.ru>
2017-10-19 00:49:55 +1000

    Repository created

---
+++ src/test/java/ru/khv/fox/software/web/cisco/restbox/app_java/AppJavaApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.khv.fox.software.web.cisco.restbox.app_java;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppJavaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
