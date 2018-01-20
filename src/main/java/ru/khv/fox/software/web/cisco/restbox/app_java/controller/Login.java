08beb5f11e67cdd70e6745c75105f0c9b2dc5fa4
Boris Fox <boris@fox.khv.ru>
2018-01-20 20:17:22 +1000

    Transient commit

---
+++ src/main/java/ru/khv/fox/software/web/cisco/restbox/app_java/controller/Login.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2018 Boris Fox.
+ * All rights reserved.
+ */
+
+package ru.khv.fox.software.web.cisco.restbox.app_java.controller;
+
+/*
+@Slf4j
+@RestController
+public class Login {
+
+	// TODO should return 201 and JWT token in "token" field
+	// or 401 and "authentication failed" in "message".
+	@RequestMapping(path = "/login", method = RequestMethod.POST)
+	public String checkLogin(@RequestBody final LoginParameters loginParameters) {
+		log.debug("username = {}, password = {}", loginParameters.getUsername(), loginParameters.getPassword());
+		// TODO заменить
+		return "test";
+	}
+
+	// обработка исключений:
+	// https://stackoverflow.com/questions/24292373/spring-boot-rest-controller-how-to-return-different-http-status-codes
+	// https://spring.io/guides/tutorials/bookmarks/
+	// http://engineering.pivotal.io/post/must-know-spring-boot-annotations-controllers/
+
+	@Data
+	@Validated
+	private static class LoginParameters {
+		@NotEmpty
+		private String username;
+		@NotEmpty
+		private String password;
+	}
+}
+*/
--- src/main/java/ru/khv/fox/software/web/cisco/restbox/app_java/controller/Routing.java
+++ src/main/java/ru/khv/fox/software/web/cisco/restbox/app_java/controller/Routing.java
@@ -1,23 +1,17 @@
 /*
- * Copyright (c) 2017 Boris Fox.
+ * Copyright (c) 2018 Boris Fox.
  * All rights reserved.
  */
 
 package ru.khv.fox.software.web.cisco.restbox.app_java.controller;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.ServerResponse;
-
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-
 /**
  * Web controller routing component.
+ * WebFlux.fn style.
  */
-@Component
-public final class Routing {
+/*
+@Configuration
+public class Routing {
 	// Routing function
 	@Bean
 	public static RouterFunction<ServerResponse> routingFunction() {
@@ -26,6 +20,8 @@ public final class Routing {
 
 		// Setup routing
 		//return route(path("/"), hello);
-		return route(GET("/"), Handlers::helloHandler);
+		//return route(GET("/"), Handlers::helloHandler);
+		return route(GET("/").and(accept(APPLICATION_JSON)), Handlers::helloHandler);
 	}
 }
+*/
